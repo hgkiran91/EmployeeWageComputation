3924167d194aae7e098628a48e72f58e8d963321
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-20 17:04:51 +0530

    Added: Employee Attendance Functionality

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,5 +3,17 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        //Check Employee Attendance
+        int empPresent = 1;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+
+        System.out.println(empCheck);
+
+        if (empCheck == empPresent) {
+            System.out.println("Employee Present");
+        } else {
+            System.out.println("Employee Absent");
+        }
     }
 }
