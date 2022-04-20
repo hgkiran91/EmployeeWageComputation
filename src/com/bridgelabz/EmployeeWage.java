e2938f7da287157763288d8dd7f7871158346b0a
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-04-20 17:28:30 +0530

    Added: EmployeeDailyWage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,14 +6,22 @@ public class EmployeeWage {
 
         //Check Employee Attendance
         int empPresent = 1;
+        int empHours=0;
+        int empWage=0;
+        int EMP_RATE_PER_HOUR=20;
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
 
         System.out.println(empCheck);
 
         if (empCheck == empPresent) {
             System.out.println("Employee Present");
-        } else {
-            System.out.println("Employee Absent");
+            empHours=8;
         }
+        else {
+            System.out.println("Employee Absent");
+            empHours=0;
+        }
+        empWage=empHours*EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage: "+empWage);
     }
 }
